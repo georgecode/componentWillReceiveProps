df5f9fda2fc3687c0e647b63196626bb2d59427f
georgecode <georgeczurlanis@gmail.com>
2017-05-18 14:30:35 -0700

    Add example of shouldComponentUpdate (but seems to do nothing)

--- app/components/App.js
+++ app/components/App.js
@@ -1,22 +1,82 @@
 var React = require('react');
 
-class Test extends React.Component{
+// class Test extends React.Component{
+// 	render(){
+// 		return(
+// 			<div>
+// 			<h1>zzzHELLO</h1>
+// 			</div>
+// 			)
+// 	}
+// }
+
+
+// module.exports = class extends React.Component{
+// 	render(){
+// 		return(
+// 			<div>
+// 				<Test />
+// 			</div>
+// 			)
+// 	}
+// }
+
+class Example extends React.Component{
+	constructor(props,context){
+		super(props, context);
+
+		this.state = {
+			title:'THIS is inital state'
+		};
+
+		this.clickShit=this.clickShit.bind(this);
+	}//END ES6 way	
+
+	shouldComponentUpdate(nextProps, nextState){
+	    if ((this.props.text == nextProps.text) && 
+      (this.state.subtext == nextState.subtext)) {
+      alert("Props and state haven't changed, so I'm not gonna update!");
+      return false;
+    	} else {
+      alert("Okay fine I will update.")
+      return true;
+    	}		
+
+	}
+
+	clickShit(){
+  		console.log("is this even working??")
+  		console.log(this.props.text)
+  		this.state.subtext = "WTF WTF";
+  		console.log(this.props.text)
+	}
+
 	render(){
 		return(
-			<div>
-			<h1>zzzHELLO</h1>
-			</div>
+	      <div>
+        	<h1 onClick={this.clickShit}>{this.props.text}</h1>
+        	<h2>{this.state.subtext}</h2>
+      	  </div>		
 			)
 	}
-}
+
+}//class Example end
 
 
 module.exports = class extends React.Component{
 	render(){
 		return(
 			<div>
-				<Test />
+				<Example text="this does nothing" />
 			</div>
 			)
 	}
 }
+
+
+
+
+
+
+
+
